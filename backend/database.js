93f2ddd240a602d772053da102cfb69c335c88c0
Gustavo N Bomfim <mifmobng@gmail.com>
2024-10-23 12:50:33 -0300

    backend development

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,51 @@
+const readline  = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+  
+function askQuestion(query) {
+    return new Promise(resolve => {
+        rl.question(query, answer => {
+            resolve(answer);
+        });
+    });
+};
+
+async function println(x){
+    return console.log(x);
+}
+async function add(newUsers){
+    let user = [];
+    for (i = 0; i < newUsers; i++) {
+        let username = await askQuestion("Insert username: ");
+        let salt = await askQuestion("Insert salt: ");
+        let psw = await askQuestion("Insert psw hashed: ");
+
+        user.push(
+            {
+                username:username,
+                salt:salt,
+                hash_psw:psw,
+            }
+        )
+    }
+    console.log(user)
+    /*
+    try  {
+        const insertManyResult = await collection.insertMany(user);
+        return console.log(`${insertManyResult.insertedCount} documents successfully inserted \n`);
+    } catch (err) {
+        return console.error(`Something went wrong trying to insert the new documents:  ${err}\n`)
+    }
+    */
+}
+ 
 const { MongoClient } = require("mongodb");
 
 async function run() { 
-    const url = "mongodb+srv://gugsgod:123454321@cluster111.qppkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster111";
+    const url = "";
 
     const client = new MongoClient(url);
 
@@ -26,13 +70,8 @@ async function run() {
         }
     ]
 
-    try  {
-        const insertManyResult = await collection.insertMany(user);
-        console.log(`${insertManyResult.insertedCount} documents successfully inserted \n`);
-    } catch (err) {
-        console.error(`Something went wrong trying to insert the new documents:  ${err}\n`)
-    }
+    
 
 }
 
-run().catch(console.dir);
+add(3);
