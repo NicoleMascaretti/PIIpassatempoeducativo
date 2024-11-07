7bf8b2fb0b3e7e64a4298abf0e6ce55bbe1ccf76
Gustavo N Bomfim <mifmobng@gmail.com>
2024-11-07 10:59:45 -0300

    oi

--- backend/main.js
+++ backend/main.js
@@ -5,14 +5,12 @@ const app = express();
 const mongoose  = require('mongoose')
 app.use(express.json());
 
-app.get('/hey', (req,res) => {
-    res.send('hey');
-});
+
 
 app.listen(3000, () => {
     try {
     mongoDbConnect()
-    console.log("up and running")
+    console.log("Server  Online")
     } 
     catch (e){
         console.log("Erro:", e)
@@ -29,19 +27,12 @@ async function mongoDbConnect(){
     mongoose.connect("mongodb+srv://gugsgod:123454321@cluster111.qppkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster111")
 }
 
-let filmes = [
-{
-titulo: "Forrest Gump - O Contador de Histórias",
-sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções."
-},
-{
-titulo: "Um Sonho de Liberdade",
-sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-}
-];
-
 app.get('/filmes', (req, res) => {
     res.json(filmes);
+});
+
+app.get('/hey', (req,res) => {
+    res.send('hey');
 });
 
 app.post("/filmes", (req,res) => {
