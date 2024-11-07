1681a96afa9d90b9e4141eee0722374dc012ffb4
Gustavo N Bomfim <mifmobng@gmail.com>
2024-11-07 12:07:31 -0300

    organizacao de diretorio

---
+++ 1_frontend/1.1_pages/1.1.1_home/navbar.js
@@ -0,0 +1,36 @@
+class MobileNavBar {
+    constructor (mobileMenu,navList){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        console.log(this);
+        this.navList.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+
+    }
+
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+        
+    }
+}
+const MobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+MobileNavBar.navList
