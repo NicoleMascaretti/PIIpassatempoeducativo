8a1197186c2aecaaf8c39da9ca5bd621518b3ed5
Gustavo N Bomfim <mifmobng@gmail.com>
2024-11-11 19:54:38 -0300

    backend

--- 2_backend/main.js
+++ 2_backend/main.js
@@ -1,48 +1,24 @@
 // REST API
 
-const express = require('express');
-const app = express();
-const mongoose  = require('mongoose')
-app.use(express.json());
-
-
+require("dotenv").config();
+const express = require ('express')
+const app = express()
+const mongoose = require('mongoose')
+app.use(express.json())
 
 app.listen(3000, () => {
-    try {
-    mongoDbConnect()
-    console.log("Server  Online")
-    } 
-    catch (e){
-        console.log("Erro:", e)
+        try {
+            db_connect()
+            console.log("App running")
+        } catch (error) {
+            console.log("ERRO: ", error)
+        }
     }
+)
 
-})
+const connect_string = process.env.CONNECT_DB
 
-const Filmes = mongoose.model ("Filme", mongoose.Schema({
-    titulo: {type: String},
-    sinopse: {type: String}
-}))
-
-async function mongoDbConnect(){
-    mongoose.connect("mongodb+srv://gugsgod:123454321@cluster111.qppkx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster111")
+async function db_connect () {
+    await
+    mongoose.connect(connect_string)
 }
-
-app.get('/filmes', (req, res) => {
-    res.json(filmes);
-});
-
-app.get('/hey', (req,res) => {
-    res.send('hey');
-});
-
-app.post("/filmes", (req,res) => {
-    // pega dados do cliente
-    const titulo = req.body.titulo;
-    const sinopse = req.body.sinopse;
-
-    const filme = {titulo: titulo, sinopse: sinopse};
-
-    filmes.push(filme);
-
-    res.json(filmes);
-});
